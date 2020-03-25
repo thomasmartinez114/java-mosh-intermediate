b919273a284563bb84567b94cfbf98f049c25bf9
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-24 22:40:13 -0400

    Encapsulation - created Employee object & set values of the employee fields

--- 02-classes/HelloWorld/src/com/martinez/Main.java
+++ 02-classes/HelloWorld/src/com/martinez/Main.java
@@ -3,6 +3,11 @@ package com.martinez;
 public class Main {
 
     public static void main(String[] args) {
+        var employee = new Employee();
+        employee.baseSalary = 50_000;
+        employee.hourlyRate = 20;
+        int wage = employee.calculateWage(10);
+        
         int baseSalary = 50_000;
         int extraHours = 10;
         int hourlyRate = 20;
