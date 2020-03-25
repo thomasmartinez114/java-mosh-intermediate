9f723f89c5cf5217dfc2a24d7459eab1e0a372f4
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-24 22:41:07 -0400

    Encapsulation - removed method from before since no longer needed - fewer lines of code now

--- 02-classes/HelloWorld/src/com/martinez/Main.java
+++ 02-classes/HelloWorld/src/com/martinez/Main.java
@@ -7,20 +7,7 @@ public class Main {
         employee.baseSalary = 50_000;
         employee.hourlyRate = 20;
         int wage = employee.calculateWage(10);
-        
-        int baseSalary = 50_000;
-        int extraHours = 10;
-        int hourlyRate = 20;
 
-        int wage = calculateWage(baseSalary, extraHours, hourlyRate);
         System.out.println(wage);
-    }
-
-    public static int calculateWage(
-            int baseSalary,
-            int extraHours,
-            int hourlyRate
-    ) {
-        return baseSalary + (extraHours * hourlyRate);
     }
 }
