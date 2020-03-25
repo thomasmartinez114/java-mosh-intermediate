9826a4f697e9e64625ebd4e0106165c2e986b4e9
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-24 21:46:51 -0400

    Memory Allocation - both reference the same Heap - since textBox2 = textBox1

--- 02-classes/HelloWorld/src/com/martinez/Main.java
+++ 02-classes/HelloWorld/src/com/martinez/Main.java
@@ -1,14 +1,11 @@
 package com.martinez;
 
+import org.w3c.dom.Text;
+
 public class Main {
 
     public static void main(String[] args) {
-        var textBox1 = new TextBox();
-        textBox1.setText("Box 1");
-        System.out.println(textBox1.text.toUpperCase());
-
-        var textBox2 = new TextBox();
-        textBox2.setText("Box 2");
-        System.out.println(textBox2.text);
+        var textBox1 = new Text();
+        var textBox2 = textBox1;
     }
 }
