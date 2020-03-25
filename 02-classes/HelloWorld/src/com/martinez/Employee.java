15df1c842d6fbea152a5ed61cfb85f97abe6b95b
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-24 22:37:30 -0400

    Encapsulation - declared calculateWage mthod - has no params because encapsulated in single object

--- 02-classes/HelloWorld/src/com/martinez/Employee.java
+++ 02-classes/HelloWorld/src/com/martinez/Employee.java
@@ -4,4 +4,9 @@ public class Employee {
     public int baseSalary;
     public int hourlyRate;
     public int extraHours;
+
+    // Declare calculateWage method
+    public int calculateWage() {
+        return baseSalary + (hourlyRate * extraHours);
+    }
 }
