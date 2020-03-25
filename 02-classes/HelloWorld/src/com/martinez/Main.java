d44edc362e81a502232f12705efeb5ac373e2bd9
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-24 21:38:15 -0400

    Creating Objects - setText

--- 02-classes/HelloWorld/src/com/martinez/Main.java
+++ 02-classes/HelloWorld/src/com/martinez/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         var textBox1 = new TextBox();
+        textBox1.setText("Box 1");
+        System.out.println(textBox1.text);
     }
 }
