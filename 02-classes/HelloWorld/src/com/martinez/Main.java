63f4818cce330f9cf4fa1c5a2ded79e132ff58a5
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-24 21:43:35 -0400

    Created 2nd object

--- 02-classes/HelloWorld/src/com/martinez/Main.java
+++ 02-classes/HelloWorld/src/com/martinez/Main.java
@@ -4,6 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         var textBox1 = new TextBox();
+        textBox1.setText("Box 1");
         System.out.println(textBox1.text.toUpperCase());
+
+        var textBox2 = new TextBox();
+        textBox2.setText("Box 2");
+        System.out.println(textBox2.text);
     }
 }
