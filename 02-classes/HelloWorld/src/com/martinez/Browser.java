e89b7b0a4a130d85bc828c8e15d877b488cf8690
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-26 23:24:50 -0400

    Reducing Coupling - initialized findIpAddress(address) method

--- 02-classes/HelloWorld/src/com/martinez/Browser.java
+++ 02-classes/HelloWorld/src/com/martinez/Browser.java
@@ -2,6 +2,10 @@ package com.martinez;
 
 public class Browser {
     public void navigate(String address) {
-        
+        String ip = findIpAddress(address);
+    }
+
+    public String findIpAddress(address) {
+        return "127.0.0.1";
     }
 }
