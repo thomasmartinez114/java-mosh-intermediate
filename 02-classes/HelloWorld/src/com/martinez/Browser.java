3c6e64da63886aae0dcd2081f593e986bff0aed1
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-26 23:27:12 -0400

    Reducing Coupling - initialized sendHttpRequest(String ip)

--- 02-classes/HelloWorld/src/com/martinez/Browser.java
+++ 02-classes/HelloWorld/src/com/martinez/Browser.java
@@ -3,6 +3,11 @@ package com.martinez;
 public class Browser {
     public void navigate(String address) {
         String ip = findIpAddress(address);
+        String html = sendHttpRequest(ip);
+    }
+
+    public String sendHttpRequest(String ip) {
+        return "<html></html>";
     }
 
     public String findIpAddress(address) {
