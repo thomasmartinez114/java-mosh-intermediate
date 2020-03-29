0211944ad2a5557aff74d6c3751f986984e8d53d
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-28 22:27:55 -0400

    Extracting Calculating classes - Find Usages of where the MortageCalculator may be used - used in MortageReport - so updated the reference calling

--- 03-refactoring-towards-an-object-oriented-design/MortageCalculator/src/com/martinez/MortageReport.java
+++ 03-refactoring-towards-an-object-oriented-design/MortageCalculator/src/com/martinez/MortageReport.java
@@ -3,8 +3,12 @@ package com.martinez;
 import java.text.NumberFormat;
 
 public class MortageReport {
+
+    private static MortageCalculator calculator;
+
     private static void printMortage(int principal, float annualInterest, byte years) {
-        double mortage = Main.calculateMortage(principal, annualInterest, years);
+        calculator = new MortageCalculator(principal, annualInterest, years);
+        double mortage = calculator(principal, annualInterest, years);
         String formattedMortage = NumberFormat.getCurrencyInstance().format(mortage);
         System.out.println();
         System.out.println("MORTAGE");
@@ -17,7 +21,7 @@ public class MortageReport {
         System.out.println("PAYMENT SCHEDULE");
         System.out.println("-----------");
         for (short month = 1; month <= years * Main.MONTHS_IN_YEAR; month++) {
-            double balance = Main.calculateBalance(principal, annualInterest, years, month);
+            double balance = calculator(principal, annualInterest, years, month);
             System.out.println(NumberFormat.getCurrencyInstance().format(balance));
         }
     }
