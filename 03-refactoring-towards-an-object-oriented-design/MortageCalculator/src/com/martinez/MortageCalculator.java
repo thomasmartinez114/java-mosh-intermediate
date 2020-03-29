2921c7eb0a3796c22832f7e77df2a5924391b7d9
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-28 22:01:17 -0400

    Extracting Calculating classes - moved calculateBalance() & calculateMortage() to MortageCalculator

--- 03-refactoring-towards-an-object-oriented-design/MortageCalculator/src/com/martinez/MortageCalculator.java
+++ 03-refactoring-towards-an-object-oriented-design/MortageCalculator/src/com/martinez/MortageCalculator.java
@@ -1,4 +1,35 @@
 package com.martinez;
 
 public class MortageCalculator {
+    public static double calculateBalance(
+            int principal,
+            float annualInterest,
+            byte years,
+            short numOfPaymentsMade // short because # can be over 300 - byte can only store 256
+    ) {
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+        short numPayments = (short) (MONTHS_IN_YEAR * years);
+
+        // B = L[(1 + c)^n - (1 + c)^p] / [(1 + c)^n - 1]
+        double balance = principal
+                * (Math.pow(1 + monthlyInterest, numPayments) - Math.pow(1 + monthlyInterest, numOfPaymentsMade))
+                / (Math.pow(1 + monthlyInterest, numPayments) - 1);
+
+        return balance;
+    }
+
+    public static double calculateMortage(
+            int principal,
+            float annualInterest,
+            byte years) {
+
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+        short numPayments = (short) (MONTHS_IN_YEAR * years);
+
+        double mortage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numPayments)
+                / (Math.pow(1 + monthlyInterest, numPayments) - 1));
+
+        return mortage;
+    }
 }
