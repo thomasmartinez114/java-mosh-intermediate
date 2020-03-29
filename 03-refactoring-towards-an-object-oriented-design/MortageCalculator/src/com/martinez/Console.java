00483adc07eed95b8f74ff00b5f898525f5c6f35
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-28 21:14:56 -0400

    Method Overloading - declared Scanner object as a Static field - so new instance of Scanner obj isnt created

--- 03-refactoring-towards-an-object-oriented-design/MortageCalculator/src/com/martinez/Console.java
+++ 03-refactoring-towards-an-object-oriented-design/MortageCalculator/src/com/martinez/Console.java
@@ -3,14 +3,13 @@ package com.martinez;
 import java.util.Scanner;
 
 public class Console {
+    private static Scanner scanner = new Scanner(System.in);
 
     public static double readNumber(String prompt) {
-        Scanner scanner = new Scanner(System.in);
         return scanner.nextDouble();
     }
 
     public static double readNumber(String prompt, double min, double max) {
-        Scanner scanner = new Scanner(System.in);
         double value;
         while (true) {
             System.out.print(prompt);
