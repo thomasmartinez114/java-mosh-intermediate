055ca6daf26cef1fefa02ba5afc92c6ea91f47cf
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-28 21:38:55 -0400

    Extracting printing classes - added Main. infront of the MONTHS_IN_YEAR & calculatebalance() since it referencing it from the Main class

--- 03-refactoring-towards-an-object-oriented-design/MortageCalculator/src/com/martinez/MortageReport.java
+++ 03-refactoring-towards-an-object-oriented-design/MortageCalculator/src/com/martinez/MortageReport.java
@@ -16,8 +16,8 @@ public class MortageReport {
         System.out.println();
         System.out.println("PAYMENT SCHEDULE");
         System.out.println("-----------");
-        for (short month = 1; month <= years * MONTHS_IN_YEAR; month++) {
-            double balance = calculateBalance(principal, annualInterest, years, month);
+        for (short month = 1; month <= years * Main.MONTHS_IN_YEAR; month++) {
+            double balance = Main.calculateBalance(principal, annualInterest, years, month);
             System.out.println(NumberFormat.getCurrencyInstance().format(balance));
         }
     }
