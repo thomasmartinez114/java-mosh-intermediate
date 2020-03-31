bc6612dcac776f1dc6a8b0e700089d8a5d6435a1
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-30 23:11:48 -0400

    Inheritance - enable() and disable()

--- 04-inheritance/HelloWorld/src/com/martinez/UIControl.java
+++ 04-inheritance/HelloWorld/src/com/martinez/UIControl.java
@@ -2,4 +2,12 @@ package com.martinez;
 
 public class UIControl {
     private boolean isEnabled = true;
+
+    public void enable() {
+        isEnabled = true;
+    }
+
+    public void disable() {
+        isEnabled = false;
+    }
 }
