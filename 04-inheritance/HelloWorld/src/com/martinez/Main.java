67a05199784e75818aece8cec59515d28650b3d6
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-31 00:12:52 -0400

    Inheritance - in Main - doing control. you can see TextBox() inherited features from UIControl()

--- 04-inheritance/HelloWorld/src/com/martinez/Main.java
+++ 04-inheritance/HelloWorld/src/com/martinez/Main.java
@@ -3,7 +3,8 @@ package com.martinez;
 public class Main {
 
     public static void main(String[] args) {
-        var control = new UIControl();
+        var control = new TextBox();
+        control.
         control.disable();
         System.out.println(control.isEnabled());
     }
