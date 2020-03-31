abffcc4d6375919238ded614fb93317a924a2031
thomasmartinez114 <tmartinez114@gmail.com>
2020-03-30 22:53:35 -0400

    Copied MortgageCalculator over - started HelloWorld

---
+++ 04-inheritance/HelloWorld/src/com/martinez/Main.java
@@ -0,0 +1,8 @@
+package com.martinez;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 04-inheritance/HelloWorld/src/com/martinez/UIControl.java
@@ -0,0 +1,5 @@
+package com.martinez;
+
+public class UIControl {
+    private boolean isEnabled = true;
+}
