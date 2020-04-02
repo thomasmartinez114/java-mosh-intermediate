017878ade62c41e33dbf3495307f6132a2a6383c
thomasmartinez114 <tmartinez114@gmail.com>
2020-04-01 23:18:26 -0400

    Object Class - .toString() - returns string representation of obj - has 2 parts - 1) Fully qualified name of class 2) hashcode in Hexadecimal

--- 04-inheritance/HelloWorld/src/com/martinez/Main.java
+++ 04-inheritance/HelloWorld/src/com/martinez/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         var box1 = new TextBox();
         var box2 = new TextBox();
-        System.out.println(box1.equals(box2));
+        System.out.println(box1.toString());
     }
 }
