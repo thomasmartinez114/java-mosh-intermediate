2aaa29cb085c171db85e822fa2a3f1466246e596
thomasmartinez114 <tmartinez114@gmail.com>
2020-04-01 23:15:35 -0400

    Object Class - .equals()

--- 04-inheritance/HelloWorld/src/com/martinez/Main.java
+++ 04-inheritance/HelloWorld/src/com/martinez/Main.java
@@ -4,8 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         var box1 = new TextBox();
-        var box2 = box1;
-        System.out.println(box1.hashCode());
-        System.out.println(box2.hashCode());
+        var box2 = new TextBox();
+        System.out.println(box1.equals(box2));
     }
 }
