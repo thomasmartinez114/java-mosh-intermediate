2bc7472f2ba86bbfc32eb63bc814526dd8a6810d
thomasmartinez114 <tmartinez114@gmail.com>
2020-04-04 00:04:48 -0400

    Constructors and Inheritance - created new TextBox obj and it calls/prints

--- 04-inheritance/HelloWorld/src/com/martinez/Main.java
+++ 04-inheritance/HelloWorld/src/com/martinez/Main.java
@@ -3,8 +3,6 @@ package com.martinez;
 public class Main {
 
     public static void main(String[] args) {
-        var box1 = new TextBox();
-        var box2 = new TextBox();
-        System.out.println(box1.toString());
+        var textbox = new TextBox();
     }
 }
