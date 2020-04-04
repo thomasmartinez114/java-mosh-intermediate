8085928b91a6228793142520d9811d4e9618b7b5
thomasmartinez114 <tmartinez114@gmail.com>
2020-04-04 00:03:24 -0400

    Constructors and Inheritance - generated TextBox() constructor

--- 04-inheritance/HelloWorld/src/com/martinez/TextBox.java
+++ 04-inheritance/HelloWorld/src/com/martinez/TextBox.java
@@ -3,6 +3,10 @@ package com.martinez;
 public class TextBox extends UIControl {
     private String text = "";
 
+    public TextBox() {
+        System.out.println("TextBox");
+    }
+
     public void setText(String text) {
         this.text = text;
     }
