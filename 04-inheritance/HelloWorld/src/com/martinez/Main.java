811a0f59781775399200e36ddf10298791780600
thomasmartinez114 <tmartinez114@gmail.com>
2020-04-05 20:08:03 -0400

    Upcasting and Downcasting - declared show method

--- 04-inheritance/HelloWorld/src/com/martinez/Main.java
+++ 04-inheritance/HelloWorld/src/com/martinez/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         var control = new UIControl(true);
+        show(control);
+    }
+
+    public static void show(UIControl control) {
+        System.out.println(control);
     }
 }
