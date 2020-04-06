d46252ef2ee07a7afb232e367662ef8f8422139e
thomasmartinez114 <tmartinez114@gmail.com>
2020-04-05 20:07:10 -0400

    Upcasting and Downcasting - delcared new UIControl obj

--- 04-inheritance/HelloWorld/src/com/martinez/Main.java
+++ 04-inheritance/HelloWorld/src/com/martinez/Main.java
@@ -3,8 +3,6 @@ package com.martinez;
 public class Main {
 
     public static void main(String[] args) {
-        var textBox = new TextBox();
-        textBox.setText("Hello World");
-        System.out.println(textBox);
+        var control = new UIControl(true);
     }
 }
