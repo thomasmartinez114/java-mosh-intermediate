1b913f3efc8a1577f66e34702a933c44eb81f57c
thomasmartinez114 <tmartinez114@gmail.com>
2020-04-14 00:50:28 -0400

    Polymorphism - create array of controls - but doing the for loop with else if will be too bloated of code - this is when polymorphism comes into play

--- 04-inheritance/HelloWorld/src/com/martinez/Main.java
+++ 04-inheritance/HelloWorld/src/com/martinez/Main.java
@@ -3,9 +3,13 @@ package com.martinez;
 public class Main {
 
     public static void main(String[] args) {
-        var point1 = new Point(1, 2);
-        var point2 = new Point(1, 2);
-        System.out.println(point1.hashCode());
-        System.out.println(point2.hashCode());
+      // Create array of control objs and render each to the console
+        UIControl[] controls = { new TextBox(), new CheckBox() };
+        for (var control : controls) {
+            // if control is a TextBox
+            //  renderTextBox
+            // else if it is a CheckBox
+            //  renderCheckBox
+        }
     }
 }
