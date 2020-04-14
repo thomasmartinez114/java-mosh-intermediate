f45dad2926e9e07cffbe5af5124cdc0f29e61cb6
thomasmartinez114 <tmartinez114@gmail.com>
2020-04-14 01:37:29 -0400

    Polymorphism - Main class rendering each version of the render to the console

--- 04-inheritance/HelloWorld/src/com/martinez/Main.java
+++ 04-inheritance/HelloWorld/src/com/martinez/Main.java
@@ -6,10 +6,7 @@ public class Main {
       // Create array of control objs and render each to the console
         UIControl[] controls = { new TextBox(), new CheckBox() };
         for (var control : controls) {
-            // if control is a TextBox
-            //  renderTextBox
-            // else if it is a CheckBox
-            //  renderCheckBox
+            control.render();
         }
     }
 }
