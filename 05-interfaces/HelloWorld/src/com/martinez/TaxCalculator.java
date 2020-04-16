b79456b00afa7e0b08d9318d531976b55aa18f57
thomasmartinez114 <tmartinez114@gmail.com>
2020-04-16 01:16:00 -0400

    Tightly-coupled Code - init TaxCalculator constructor

--- 05-interfaces/HelloWorld/src/com/martinez/TaxCalculator.java
+++ 05-interfaces/HelloWorld/src/com/martinez/TaxCalculator.java
@@ -3,5 +3,7 @@ package com.martinez;
 public class TaxCalculator {
     private double taxableIncome;
 
-    
+    public TaxCalculator(double taxableIncome) {
+        this.taxableIncome = taxableIncome;
+    }
 }
