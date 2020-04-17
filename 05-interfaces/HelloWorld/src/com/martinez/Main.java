4a8674237c21d0a2bfa1fa2d6cae30a4c1e4be85
thomasmartinez114 <tmartinez114@gmail.com>
2020-04-17 01:15:10 -0400

    Method Injection - passed the depenedency to method that needs the dependcy

--- 05-interfaces/HelloWorld/src/com/martinez/Main.java
+++ 05-interfaces/HelloWorld/src/com/martinez/Main.java
@@ -4,10 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         var calculator = new TaxCalculator2020(100_000);
-        var report = new TaxReport(calculator);
-        report.show();
-
-        report.setCalculator(new TaxCalculator2021());
-        report.show();
+        var report = new TaxReport();
+        report.show(calculator);
+        report.show(new TaxCalculator2021());
     }
 }
