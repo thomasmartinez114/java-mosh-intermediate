7b9240c679d3856f8690c8c57a9dce62317b3e97
thomasmartinez114 <tmartinez114@gmail.com>
2020-04-17 00:53:51 -0400

    Constructor Injection - passed it along to the Main

--- 05-interfaces/HelloWorld/src/com/martinez/Main.java
+++ 05-interfaces/HelloWorld/src/com/martinez/Main.java
@@ -3,6 +3,7 @@ package com.martinez;
 public class Main {
 
     public static void main(String[] args) {
-
+        var calculator = new TaxCalculator2020(100_000);
+        var report = new TaxReport(calculator);
     }
 }
