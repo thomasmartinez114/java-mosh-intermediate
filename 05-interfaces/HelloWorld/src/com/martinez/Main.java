51249507db5e37e7dc40314f8f80433207fa365f
thomasmartinez114 <tmartinez114@gmail.com>
2020-04-17 01:08:08 -0400

    Setter Injection - throughout lifetime of program can change the dependcy of the class

--- 05-interfaces/HelloWorld/src/com/martinez/Main.java
+++ 05-interfaces/HelloWorld/src/com/martinez/Main.java
@@ -5,5 +5,9 @@ public class Main {
     public static void main(String[] args) {
         var calculator = new TaxCalculator2020(100_000);
         var report = new TaxReport(calculator);
+        report.show();
+
+        report.setCalculator(new TaxCalculator2021());
+        report.show();
     }
 }
