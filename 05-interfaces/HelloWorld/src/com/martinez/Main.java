630457a27ae56fb5cfb762dba9c6429cec28e0f9
thomasmartinez114 <tmartinez114@gmail.com>
2020-04-16 23:59:45 -0400

    Creating an Interface - iimplemented Interface to the TaxCalculator2020 class

--- 05-interfaces/HelloWorld/src/com/martinez/Main.java
+++ 05-interfaces/HelloWorld/src/com/martinez/Main.java
@@ -3,10 +3,6 @@ package com.martinez;
 public class Main {
 
     public static void main(String[] args) {
-      // Create array of control objs and render each to the console
-        UIControl[] controls = { new TextBox(), new CheckBox() };
-        for (var control : controls) {
-            control.render();
-        }
+
     }
 }
--- 05-interfaces/HelloWorld/src/com/martinez/TaxCalculator2020.java
+++ 05-interfaces/HelloWorld/src/com/martinez/TaxCalculator2020.java
@@ -1,12 +1,12 @@
 package com.martinez;
 
-public class TaxCalculator2020 {
+public class TaxCalculator2020 implements TaxCalculator {
     private double taxableIncome;
 
     public TaxCalculator2020(double taxableIncome) {
         this.taxableIncome = taxableIncome;
     }
-
+    
     public double calculateTax() {
         return taxableIncome * 0.4;
     }
