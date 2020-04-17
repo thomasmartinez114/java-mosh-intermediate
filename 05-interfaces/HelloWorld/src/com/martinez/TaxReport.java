a5f5bcac2eb1193031af0138c5ac4aea51e794d1
thomasmartinez114 <tmartinez114@gmail.com>
2020-04-17 00:52:19 -0400

    Constructor Injection - injected the TaxCalculator interface

--- 05-interfaces/HelloWorld/src/com/martinez/TaxReport.java
+++ 05-interfaces/HelloWorld/src/com/martinez/TaxReport.java
@@ -1,10 +1,10 @@
 package com.martinez;
 
 public class TaxReport {
-    private TaxCalculator2020 calculator;
+    private TaxCalculator calculator;
 
-    public TaxReport() {
-        calculator = new TaxCalculator2020(100_000);
+    public TaxReport(TaxCalculator calculator) {
+        this.calculator = calculator;
     }
 
     public void show() {
