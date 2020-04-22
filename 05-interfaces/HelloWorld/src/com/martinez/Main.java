c2ce5213aeff9d9c11b349ef05a09bf138208601
Thomas Martinez <tmartinez114@gmail.com>
2020-04-22 01:14:35 -0400

    Static Methods - problem with this is that the Interface has implementaiton that can evenutally change in the future.

--- 05-interfaces/HelloWorld/src/com/martinez/Main.java
+++ 05-interfaces/HelloWorld/src/com/martinez/Main.java
@@ -3,9 +3,9 @@ package com.martinez;
 public class Main {
 
     public static void main(String[] args) {
-        var calculator = new TaxCalculator2020(100_000);
-        var report = new TaxReport();
-        report.show(calculator);
-        report.show(new TaxCalculator2021());
+    }
+
+    public void doSomething(TaxCalculator calculator) {
+        TaxCalculator.minimumTax
     }
 }
--- 05-interfaces/HelloWorld/src/com/martinez/TaxCalculator.java
+++ 05-interfaces/HelloWorld/src/com/martinez/TaxCalculator.java
@@ -3,4 +3,8 @@ package com.martinez;
 public interface TaxCalculator {
     float minimumTax = 100;
     double calculateTax();
+
+    static double getTaxableIncome(double income, double expenses) {
+        return income - expenses;
+    }
 }
