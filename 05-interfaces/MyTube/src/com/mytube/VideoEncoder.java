d66f5a999b9d5f23c3dfb2b8d2108483be5f6906
Thomas Martinez <tmartinez114@gmail.com>
2020-04-21 23:55:00 -0400

    Project solution - updated VideoEncode.java

--- 05-interfaces/MyTube/src/com/mytube/VideoEncoder.java
+++ 05-interfaces/MyTube/src/com/mytube/VideoEncoder.java
@@ -1,5 +1,8 @@
 package com.mytube;
 
 public interface VideoEncoder {
-    void encode(Video video);
+    public void encode(Video video) {
+        System.out.println("Encoding video...");
+        System.out.println("Done!\n");
+    }
 }
