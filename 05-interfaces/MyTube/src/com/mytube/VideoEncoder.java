d7122d8340f6a64d6dad908ee99980235eedb0a4
Thomas Martinez <tmartinez114@gmail.com>
2020-04-21 23:57:16 -0400

    Project solution - refactored VideoEncoder to XVideoEncoder

--- 05-interfaces/MyTube/src/com/mytube/VideoEncoder.java
+++ 05-interfaces/MyTube/src/com/mytube/VideoEncoder.java
@@ -1,8 +1,5 @@
 package com.mytube;
 
 public interface VideoEncoder {
-    public void encode(Video video) {
-        System.out.println("Encoding video...");
-        System.out.println("Done!\n");
-    }
+    void encode(Video video);
 }
